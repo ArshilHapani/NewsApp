d0252d46f4cf93cfff25e74ca6c73c274b494320
ArshilHapani <arshilhapani998@gmail.com>
2022-12-10 22:54:26 +0530

    First Commit of the news app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+import Navbar from './Components/navbar';
 
 //! To make quick react class based component type 'rcc'
- import React, { Component } from 'react'
- 
- export default class App extends Component {
-  name = 'Arshil' //? We can define direct class variable without using let or var
-   render() {
-     return (
-       <div>App {this.name}</div>
-     )
-   }
- }
- 
+import React, { Component } from 'react'
+import News from './Components/News';
+
+export default class App extends Component {
+  // name = 'Arshil' //? We can define direct class variable without using let or var
+  render() {
+    return (
+      <>        
+        <Navbar />
+        <News/>
+      </>
+    )
+  }
+}
+
+
+
