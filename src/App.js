b003126743a8473ef0d129d45646ad5f3710988f
ArshilHapani <arshilhapani998@gmail.com>
2022-12-17 23:34:47 +0530

    Top Navbar Added...

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 // import News from './Components/News';
 import News1 from './Components/News1';
 import PropTypes from 'prop-types'
+import LoadingBar from 'react-top-loading-bar'
 import {
   BrowserRouter as Router,
   Route,
@@ -44,21 +45,63 @@ export default class App extends Component {
         mode: 'light',
       })
     }
-  }  
+  }
   // name = 'Arshil' //? We can define direct class variable without using let or var
   //  pageSize = 8;
-  render() {    
+  state = {
+    progress:0,
+  }
+  setProgress=(progress)=>{
+    this.setState({
+      progress: progress,
+    })
+  }
+  render() {
     return (
       <>
         <div>
           {/*v//* Here we have use the router because we navigate particular component by the Link tag in navbar and then from different paths we target different category ....... Hand's off Harry 🙌  */}
           <Router>
             <Navbar ThemeToggle={this.ThemeToggle} mode={this.state.mode} />
+
+            <LoadingBar
+              color='#efebeb'
+              progress={this.state.progress}              
+            />
+
             {/* //! Here we need to do force remount component 
             //? TO do thet we use key property which is used to identify unique component names */}
             <Routes>
               <Route exact path='/'
-                element={<News1
+                element={<News1 setProgress={this.setProgress}
+                  pageSize={this.pageSize}
+                  mode={this.state.mode}
+                  country='in'
+                  category='general'
+                  key='general'
+                  height={3}
+                />}
+              />
+              <Route exact path='/business'
+                element={<News1 setProgress={this.setProgress}
+                  pageSize={this.pageSize}
+                  mode={this.state.mode}
+                  country='in'
+                  category='business'
+                  key='business'
+                />}
+              />
+              <Route exact path='/entertaiment'
+                element={<News1 setProgress={this.setProgress}
+                  pageSize={this.pageSize}
+                  mode={this.state.mode}
+                  country='in'
+                  category='entertaiment'
+                  key='entertaiment'
+                />}
+              />
+              <Route exact path='/general'
+                element={<News1 setProgress={this.setProgress}
                   pageSize={this.pageSize}
                   mode={this.state.mode}
                   country='in'
@@ -66,60 +109,33 @@ export default class App extends Component {
                   key='general'
                 />}
               />
-                <Route exact path='/business'
-                  element={<News1
-                    pageSize={this.pageSize}
-                    mode={this.state.mode}
-                    country='in'
-                    category='business'
-                    key='business'
-                  />}
-                />
-                <Route exact path='/entertaiment'
-                  element={<News1
-                    pageSize={this.pageSize}
-                    mode={this.state.mode}
-                    country='in'
-                    category='entertaiment'
-                    key='entertaiment'
-                  />}
-                />
-                <Route exact path='/general'
-                  element={<News1
-                    pageSize={this.pageSize}
-                    mode={this.state.mode}
-                    country='in'
-                    category='general'
-                    key='general'
-                  />}
-                />
-                <Route exact path='/health'
-                  element={<News1
-                    pageSize={this.pageSize}
-                    mode={this.state.mode}
-                    country='in'
-                    category='health'
-                    key='health'
-                  />}
-                />
-                <Route exact path='/science'
-                  element={<News1
-                    pageSize={this.pageSize}  
-                    mode={this.state.mode}
-                    country='in'
-                    category='science'
-                    key='science'
-                  />}
-                />
-                <Route exact path='/technology'
-                  element={<News1
-                    pageSize={this.pageSize}
-                    mode={this.state.mode}
-                    country='in'
-                    category='technology'
-                    key='technology'
-                  />}
-                />               
+              <Route exact path='/health'
+                element={<News1 setProgress={this.setProgress}
+                  pageSize={this.pageSize}
+                  mode={this.state.mode}
+                  country='in'
+                  category='health'
+                  key='health'
+                />}
+              />
+              <Route exact path='/science'
+                element={<News1 setProgress={this.setProgress}
+                  pageSize={this.pageSize}
+                  mode={this.state.mode}
+                  country='in'
+                  category='science'
+                  key='science'
+                />}
+              />
+              <Route exact path='/technology'
+                element={<News1 setProgress={this.setProgress}
+                  pageSize={this.pageSize}
+                  mode={this.state.mode}
+                  country='in'
+                  category='technology'
+                  key='technology'
+                />}
+              />
             </Routes>
           </Router>
         </div>
